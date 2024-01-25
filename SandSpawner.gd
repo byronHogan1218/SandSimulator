2673addd6965ae35d9990b620f9d3d9de8565c74
Byron Hogan <watcher0808@gmail.com>
2024-01-24 19:15:45 -0600

    sand is falling with mouse

--- SandSpawner.gd
+++ SandSpawner.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var cellSize: int = 10
+var cellSize: int = 8
 # This is the hardcoded width of the window divided by the cells size
 var windowWidth: int = -1
 # This is the hardcoded height of the window divided by the cells size
@@ -10,12 +10,17 @@ var rng = RandomNumberGenerator.new()
 
 var colors = [Color.WHITE, Color.RED, Color.GREEN, Color.BLUE]
 
-var timeBetween: float = .1
-var drawThreshold: float = .04
+var timeBetween: float = 0
+var drawThreshold: float = .001
 var instantiatedMeshes = []
+var finishedParticles = []
 var currentSand: Array = []
 var index: Array = []
-var newSand = []
+
+var rowsComplete: Array[bool] = []
+var meshInstances: Array = []
+
+var newSandTest = []
 
 var temp = 0
 # Called when the node enters the scene tree for the first time.
@@ -25,52 +30,80 @@ func _ready():
 
 	for row in range(windowHeight):
 		var sandRow = []
+		var sandRowMeshes = []
 		for column in range(windowWidth):
 			sandRow.append(null)
+			var meshInstance = MeshInstance2D.new()
+			sandRowMeshes.append(meshInstance)
+		meshInstances.append(sandRowMeshes)
 		currentSand.append(sandRow)
+		newSandTest.append(sandRow)
 	print("Column amount: ", windowWidth)
 	print("Row amount: ", windowHeight)
+	addSandToIndex(0,10)
+	connect("input_event", _on_input_event)	
 	pass # Replace with function body.
 	
-	
+
+# Function called when any input event occurs
+func _on_input_event(viewport, event, shape_idx):
+	# Check if the input event is a mouse button press
+	if event is InputEventMouseButton and event.pressed:
+		# Check if the pressed mouse button is the left button (BUTTON_LEFT)
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			
+			print("Left mouse button pressed")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_action_pressed("left"):
+		var mouse_position: Vector2 = get_global_mouse_position()
+		coordinatesToSand(mouse_position)
 	timeBetween = timeBetween + delta
-	if(timeBetween < drawThreshold):
-		return
+	#if(timeBetween < drawThreshold):
+		#return
 	timeBetween = 0
-	temp = temp + 1
-	if(temp > 10):
-		var t = 0
-		while(t<48):
-			addSandToIndex(0,t)
-			t = t + 1				
-		temp = 0	
+	#temp = temp + 1
+	#if(temp > 10):
+		#var t = 0
+		#while(t<windowWidth):
+			#addSandToIndex(0,t)
+			#t = t + 1				
+		#temp = 0	
 	for instance in instantiatedMeshes:
 		remove_child(instance)
 	instantiatedMeshes = []
-	currentSand = generateNewSand()
-
-	
+	generateNewSand()
 	drawSand()
 	pass
 	
 func drawSand():
-
-	for sandParticleRow in currentSand:
-		for sandParticle in sandParticleRow:		
-			if(sandParticle == null):
+	#for finishedSand in finishedParticles:
+		#add_child(finishedSand)
+	for row in range(windowHeight):
+	#for sandParticleRow in currentSand:
+		var sandParticleRow = currentSand[row]
+		for column in range(windowWidth):		
+		#for sandParticle in sandParticleRow:
+			var sandParticle = sandParticleRow[column]
+			if(sandParticle == null || sandParticle.isMeshSaved() && sandParticle.isFinalDestinationReached()):
 				continue
-			var meshInstance = MeshInstance2D.new()
+			var meshInstance = MeshInstance2D.new()			
+
 			meshInstance.mesh = sandParticle.getMesh()
 			meshInstance.translate(Vector2(sandParticle.getPosition().x, sandParticle.getPosition().y))
 
-			meshInstance.self_modulate = sandParticle.getColor() #Color(1, 0, 0)
+			meshInstance.self_modulate = sandParticle.getColor()
 
 			add_child(meshInstance)
-			# TODO only append sand that is not done moving
 			instantiatedMeshes.append(meshInstance)
+			
+			#if(!sandParticle.isFinalDestinationReached()):
+				#instantiatedMeshes.append(meshInstance)
+			#else:
+				#print("adding to finished")
+				#sandParticle.setMeshSaved()
+				#finishedParticles.append(meshInstance)
 
 func addSandToIndex(row: int, column: int) -> void:
 	if(sandInIndex(row,column,currentSand)):
@@ -100,24 +133,38 @@ func addSandToIndex(row: int, column: int) -> void:
 
 	var xposition = cellSize * column
 	var yposition = cellSize * row
-	var sandParticle = Sand.new(mesh, Vector2(xposition,yposition), colors[rng.randi_range(0,3)])
+	var sandParticle = Sand.new(mesh, Vector2(xposition,yposition), colors[rng.randi_range(0,colors.size()-1)],row,column)
 	currentSand[row][column] = sandParticle
 
-func generateNewSand() -> Array:
-	var newSand = currentSand.duplicate(true)
+func generateNewSand() -> void:
+	var sandToMoveDown = []
 	for row in range(windowHeight):
-		for column in range(windowWidth):
-			if(sandInIndex(row,column, currentSand)):
-				if(sandShouldMoveDown(row,column, currentSand)):
-					moveSandDown(row,column,newSand)
-	return newSand
+		if (!isRowComplete(currentSand[row])):
+			for column in range(windowWidth):
+				if(sandInIndex(row,column, currentSand)):
+					if(sandShouldMoveDown(row,column, currentSand)):
+						sandToMoveDown.append(currentSand[row][column])
+					else:
+						newSandTest[row][column].setFinalDestinationReached()
+	for sand in sandToMoveDown:
+		moveSandDown(sand.getRow(),sand.getColumn(),currentSand)
 					
+			
+func isRowComplete(sandArray: Array) -> bool:
+	return sandArray.reduce(func(accum, sand): return accum && (sand == null || sand.isFinalDestinationReached()), false)
+
+
+func coordinatesToSand(location: Vector2):
+	var row = int(location.y/cellSize)
+	var column = int(location.x/cellSize)
+	addSandToIndex(row,column)
+
 			
 func sandInIndex(row: int, column: int,sandArray) -> bool:
 	return sandArray[row][column] != null
 	
 func sandShouldMoveDown(row: int, column: int,sandArray) -> bool:
-	if(row + 1 >= windowHeight):
+	if(row + 1 > windowHeight-1):
 		return false
 	return sandArray[row + 1][column] == null
 	
@@ -126,5 +173,6 @@ func moveSandDown(row: int, column: int, sandArray) -> void:
 	sandArray[row][column] = null
 	var xposition = cellSize * column
 	var yposition = cellSize * (row + 1)
-	sandArray[row + 1][column].updatePosition(Vector2(xposition,yposition))
+	var test = sandArray[row + 1][column]
+	sandArray[row + 1][column].updatePosition(Vector2(xposition,yposition),row+1,column)
 	
--- sand.gd
+++ sand.gd
@@ -3,23 +3,46 @@ class_name Sand
 var _mesh: Mesh = null
 var _position: Vector2 = Vector2.ZERO
 var _color: Color = Color.WHITE
+var _finalDestinationReached = false
+var _meshSaved = false
+var _row = -1
+var _column = -1
 
-func _init(mesh,position,color = Color.WHITE):
+
+func _init(mesh,position,color,row,column):
 	_mesh = mesh
 	_position = position
 	_color = color
+	_row = row
+	_column = column
 	
 func getMesh() -> Mesh:
 	return _mesh
+func getRow() -> int:
+	return _row
+func getColumn() -> int:
+	return _column
 func getPosition() -> Vector2:
 	return _position
 func getColor() -> Color:
 	return _color
 	
-func updatePosition(newPosition: Vector2) -> void:
+func updatePosition(newPosition: Vector2, row: int, column: int) -> void:
+	_row = row
+	_column = column
 	_position = newPosition
 	
+func setFinalDestinationReached() -> void:
+	_finalDestinationReached = true
 
+func isFinalDestinationReached() -> bool:
+	return _finalDestinationReached
 	
-	
+func setMeshSaved() -> void:
+	_meshSaved = true
 
+func isMeshSaved() -> bool:
+	return _meshSaved
+
+func toString() -> String:
+	return "Row: " + str(_row) + ",Column: " + str(_column)
